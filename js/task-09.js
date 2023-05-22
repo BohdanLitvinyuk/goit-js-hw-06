44624889460725e220e531481c3ccba545d64a27
Bohdan Lytvyniuk <lytvyniukbohdan@gmail.com>
2023-05-22 23:41:49 +0200

    finished task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  container:document.querySelector(".widget"),
+  btn: document.querySelector(".change-color"),
+  colorName: document.querySelector(".color")
+}
+
+refs.btn.addEventListener("click", onBtnClick)
+
+function onBtnClick() {
+  let changeColor = getRandomHexColor();
+  document.body.style.backgroundColor = changeColor;
+  refs.colorName.textContent = changeColor;
+console.log(`Current color: ${changeColor}`)
+}
